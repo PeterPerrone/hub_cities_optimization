430f9fc87927df0b9ce52b70095f930b43000c10
bwoody13 <bwoody13@users.noreply.github.com>
2023-11-03 16:47:29 -0400

    update objective to use costs in hubs if defined

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ VALID_SIZES = ["small", "med", "large"]
 
 class HubModel:
 
-    def __init__(self, K=2, alpha=0.75, size="small", model_name="Hub-Spoke"):
+    def __init__(self, K=2, alpha=0.75, size="small", model_name="Hub-Spoke", c=0):
         if size not in VALID_SIZES:
             raise Exception(f"Only sizes of {VALID_SIZES} are allowed. You provided {size}.")
         self.K = K
@@ -15,6 +15,8 @@ class HubModel:
         self.df_cities, self.df_packages = load_dataframes(size)
         self.cities = self.df_cities.id.to_list()
         self.N = len(self.cities)
+
+        self.c = c
 
         self.model = gp.Model(model_name)
         self.f = extract_packages(self.df_packages, self.N)
@@ -43,8 +45,13 @@ class HubModel:
         for (s, a), pkgs in self.f.items():
             for h1 in self.cities:
                 for h2 in self.cities:
-                    obj += pkgs * self.Z[s, h1, a, h2] * \
-                           (self.d[(s, h1)] + self.alpha * self.d[(h1, h2)] + self.d[(h2, a)])
+                    if self.c:
+                        obj += pkgs * self.Z[s, h1, a, h2] * \
+                               (self.d[(s, h1)] + self.alpha * self.d[(h1, h2)] + self.d[(h2, a) +
+                                self.c * (2 - self.Y[h1, s] - self.Y[h1, h2] - self.Y[h2, a])])
+                    else:
+                        obj += pkgs * self.Z[s, h1, a, h2] * \
+                               (self.d[(s, h1)] + self.alpha * self.d[(h1, h2)] + self.d[(h2, a)])
         self.model.setObjective(obj, GRB.MINIMIZE)
 
     def create_constraints(self):
